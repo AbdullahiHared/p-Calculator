ce39990326108f2400bc2a5a0832e8c1e3ad11dc
Abdullahi <muxsinxareed6@gmail.com>
2023-09-02 16:42:47 +0200

    completed styles

---
+++ app.js
@@ -0,0 +1,24 @@
+const add = function (a,b) {
+    return a + b;
+}
+const subtract = function (a, b) {
+    return a - b;
+}
+const multiply = function (a, b) {
+    a * b;
+}
+const divide = function (a, b) {
+    return a / b;
+}
+
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+const operate = function(a, b) {
+    return divide(a,b);
+}
+
+
+
