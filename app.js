5342e383f6a35c77cf04197aa37d3180e8745ba6
Abdullahi <muxsinxareed6@gmail.com>
2023-09-04 08:22:12 +0200

    Simple commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const operate = function(a, b) {
 const display = document.querySelector('.display');
 const numOne = document.querySelector('#one');
 const numTwo = document.querySelector('#two');
-const numThree = document.querySelector('three');
+const numThree = document.querySelector('#three');
 const numFour = document.querySelector('#four');
 const numFive = document.querySelector('#five');
 const numSix = document.querySelector('#six');
@@ -39,33 +39,45 @@ const subtractKey = document.querySelector('#subtract');
 
 
 display.textContent = "";
+
 function changeNum(num) {
-    display.textContent += num;
+    return display.textContent += num;
 }
 numOne.addEventListener("click", () => {
     changeNum("1");
 });
+
 numTwo.addEventListener("click", () => {
     changeNum("2");
 });
+
 numThree.addEventListener("click", () => {
     changeNum("3");
 });
+
 numFour.addEventListener("click", () => {
     changeNum("4");
 });
+
 numFive.addEventListener("click", () => {
     changeNum("5");
 });
+
 numSix.addEventListener("click", () => {
     changeNum("6");
 });
+
 numSeven.addEventListener("click", () => {
     changeNum("7");
 });
+
 numEight.addEventListener("click", () => {
-    changeNum("9");
+    changeNum("8");
 });
+numNine.addEventListener("click", () => {
+    changeNum("9")
+})
+
 zero.addEventListener("click", () => {
     changeNum("0");
 });
