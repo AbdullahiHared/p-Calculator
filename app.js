c8df3a61a6d24080fe1d9d8972051c1a1030fe97
Muhammad Shamaeem <muhammad.shamaeem@elev.ga.ntig.se>
2023-10-09 08:01:16 +0200

    implement calculate f

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const multiply = function (a, b) {
   return a * b;
 };
 const divide = function (a, b) {
-  return a / b;
+  return (a/b).toFixed(3);
 };
 
 const topDisplay = document.querySelector(".top-display")
@@ -18,10 +18,11 @@ const operators = document.querySelectorAll(".operators");
 const clear = document.getElementById("clear")
 const result = document.getElementById("result");
 
-let equation = [];
-let currentNumber = "";
-let operatorCount = 0;
-let currentOperator = "";
+let equation = []
+let currentNumber = ""
+let operatorCount = 0
+let currentOperator = ""
+let operatorRuleCheck = false
 
 numbers.forEach((number) => {
   number.addEventListener('click', () => {
@@ -42,7 +43,7 @@ result.addEventListener('click', () => {
 })
 
 function updateDisplay(val) {
-    if (bottomDisplay.textContent.length < 17) {
+    if (bottomDisplay.textContent.length < 17 && equation.length !== 1) {
         const value = val.textContent
         if (!notOperator(value)) {
             bottomDisplay.textContent += value
@@ -63,6 +64,9 @@ function updateDisplayOperator(operator) {
             currentOperator = operator;
             equation.push(operator)
             bottomDisplay.textContent += ` ${operator} `
+            if (isOperatorRule(operator)) {
+                operatorRuleCheck = true
+            } 
         }
     }
 }
@@ -71,9 +75,21 @@ function notOperator(val) {
     return !(val !== "×" && val !== "-" && val !== "÷" && val !== "+");
 }
 
+function isOperatorRule(operator) {
+    return operator === "÷" || operator === "×";
+}
+function isOperator(operator) {
+    return operator === "+" || operator === "-";
+}
+
 function clearDisplay() {
     topDisplay.textContent = bottomDisplay.textContent
     bottomDisplay.textContent = ""
+    equation = []
+    operatorRuleCheck = false
+    currentOperator = ""
+    currentNumber = ""
+    operatorCount = 0
 }
 
 function displayResult() {
@@ -81,7 +97,60 @@ function displayResult() {
     if (equation.length > 0) {
         if (currentNumber !== "") {
             topDisplay.textContent = bottomDisplay.textContent
-            bottomDisplay.textContent = subtract(Number(equation[0]), Number(equation[2]))
+            bottomDisplay.textContent = calculate()
         }
     }
+}
+
+function calculate() {
+    console.log(equation)
+    compute()
+    return equation[0]
+}
+
+function pairComputation(firstNumber, operator, secondNumber) {
+    switch(operator) {
+        case "×":
+            return multiply(firstNumber, secondNumber)
+        case "÷":
+            return divide(firstNumber, secondNumber)
+        case "-":
+            return subtract(firstNumber, secondNumber)
+        case "+":    
+            return add(firstNumber, secondNumber)
+    }
+}
+
+
+function compute() {
+    while(equation.length !== 1) {
+        let operatorIndex = getRuleIndex()
+        let firstNumber = Number(equation[operatorIndex - 1])
+        let operator =  equation[operatorIndex]
+        let secondNumber = Number(equation[operatorIndex + 1])
+
+        if (operatorRuleCheck) {
+            const computed = pairComputation(firstNumber, operator, secondNumber)
+            console.log(computed)
+            equation.splice(operatorIndex - 1, operatorIndex + 2, computed)
+        }else {
+            const computed = pairComputation(firstNumber, operator, secondNumber)
+            console.log(computed)
+            equation.splice(operatorIndex - 1, operatorIndex + 2, computed)
+        }
+
+        console.log(equation)
+    }
+}
+
+function getRuleIndex() {
+    let index = 0;
+    equation.forEach((n, currentIndex) => {
+        if (isOperatorRule(n)) {
+            index = currentIndex
+        }else if (isOperator(n)) {
+            index = currentIndex
+        }
+    })
+    return index
 }
