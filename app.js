8d67b51ed67fa0b2ce241ecb7737b2b4dcbe73d2
Abdullahi <muxsinxareed6@gmail.com>
2023-10-09 15:28:12 +0200

    add

--- app.js
+++ app.js
@@ -1,147 +1,74 @@
+// Define basic arithmetic operations as functions
 const add = function (a, b) {
   return a + b;
 };
-function subtract(a, b) {
+const subtract = function (a, b) {
   return a - b;
 };
 const multiply = function (a, b) {
   return a * b;
 };
 const divide = function (a, b) {
-  return (a/b).toFixed(3);
+  return a / b;
 };
 
-const topDisplay = document.querySelector(".top-display")
-const bottomDisplay = document.querySelector(".bottom-display")
-const numbers = document.querySelectorAll(".numbers");
-const operators = document.querySelectorAll(".operators");
-const clear = document.getElementById("clear")
-const result = document.getElementById("result");
+// Select elements from the HTML document
+const display = document.querySelector(".display"); // Display screen
+const numbers = document.querySelectorAll(".numbers"); // Number buttons
+const operators = document.querySelectorAll(".mathKeys"); // Operator buttons
+const equals = document.getElementById("result"); // Equals button
+display.textContent = ""; // Initialize display content
 
-let equation = []
-let currentNumber = ""
-let operatorCount = 0
-let currentOperator = ""
+// Function to update the display with a number or operator
+const changeNum = function (num) {
+  // Check if the display content length is less than 25 characters
+  if (display.textContent.length < 25) {
+    display.textContent += num.textContent;
+  }
+};
+
+// Add click event listeners to number buttons
 numbers.forEach((number) => {
-  number.addEventListener('click', () => {
-      updateDisplay(number);
+  number.addEventListener("click", () => {
+    changeNum(number);
   });
 });
+
+// Add click event listeners to operator buttons
 operators.forEach((operator) => {
-  operator.addEventListener('click', () => {
-      updateDisplay(operator)
+  operator.addEventListener("click", () => {
+    changeNum(operator);
+    console.log(operator.textContent); // Log the clicked operator
   });
 });
 
-clear.addEventListener('click', () => {
-    clearDisplay()
-})
-result.addEventListener('click', () => {
-    displayResult()
-})
+// Check which operators are present in the display content and perform calculations accordingly
+function operate() {
+  // Variables to store user input
+  let splitScreen =  display.textContent.split(/[/*]/);;  
+  let firstNumber = Number(splitScreen[0]);
+  let secondNumber  = Number(splitScreen[1]);;
+  let thirdNumber  = Number(splitScreen[2]);
+  let result;
+  let divTester1 = divide(firstNumber, secondNumber) * thirdNumber;
+  let divTester2= divide(thirdNumber, secondNumber) * firstNumber;
 
-function updateDisplay(val) {
-    if (bottomDisplay.textContent.length < 17 && equation.length !== 1) {
-        const value = val.textContent
-        if (!notOperator(value)) {
-            bottomDisplay.textContent += value
-            currentNumber += value
-            currentOperator = ""
-        }else {
-            equation.push(currentNumber)
-            currentNumber = ""
-            updateDisplayOperator(value)
-        }
-    }
+   if (display.textContent.includes("/") && display.textContent.includes("*")) {
+     result = divTester1 || divTester2;
+     display.textContent = result;
+     console.log(result);
+   } 
 }
 
-function updateDisplayOperator(operator) {
-    if (equation.length > 0 && operatorCount !== 2) {
-        if (!notOperator(equation[equation.length - 1]) && currentOperator === "") {
-            operatorCount += 1
-            currentOperator = operator;
-            equation.push(operator)
-            bottomDisplay.textContent += ` ${operator} `
-        }
-    }
-}
+// function that supports operation of three operators
 
-function notOperator(val) {
-    return !(val !== "×" && val !== "-" && val !== "÷" && val !== "+");
-}
+// Function to perform calculations when the equals button is clicked
+equals.addEventListener("click", function () {
+  operate();
+});
 
-function isOperatorRule(operator) {
-    return operator === "÷" || operator === "×";
-}
-function isOperator(operator) {
-    return operator === "+" || operator === "-";
-}
-
-function clearDisplay() {
-    topDisplay.textContent = bottomDisplay.textContent
-    bottomDisplay.textContent = ""
-    equation = []
-    currentOperator = ""
-    currentNumber = ""
-    operatorCount = 0
-}
-
-function displayResult() {
-    equation.push(currentNumber)
-    if (equation.length > 0) {
-        if (currentNumber !== "") {
-            topDisplay.textContent = bottomDisplay.textContent
-            bottomDisplay.textContent = calculate()
-        }
-    }
-
-}
-
-function calculate() {
-    compute()
-    compute()
-    return equation[0]
-}
-
-function pairComputation(firstNumber, operator, secondNumber) {
-    switch(operator) {
-        case "×":
-            return multiply(firstNumber, secondNumber)
-        case "÷":
-            return divide(firstNumber, secondNumber)
-        case "-":
-            return subtract(firstNumber, secondNumber)
-        case "+":    
-            return add(firstNumber, secondNumber)
-    }
-}
-
-
-function compute() {
-    let operatorIndex = getRuleIndex()
-    let firstNumber = Number(equation[operatorIndex - 1])
-    let operator =  equation[operatorIndex]
-    let secondNumber = Number(equation[operatorIndex + 1])
-    
-    const computed = pairComputation(firstNumber, operator, secondNumber)
-    equation.splice(operatorIndex - 1, operatorIndex + 2, computed)
-}
-
-function getRuleIndex() {
-    let firstOperator = equation[1]
-    let secondOperator = equation[3]
-    
-    if (firstOperator) {
-        if (isOperator(firstOperator) && secondOperator) {
-            return 3
-        }else {
-            return 1
-        }
-    }else if (secondOperator) {
-        if (isOperator(secondOperator)) {
-            return 1
-        }else {
-            return 3
-        }
-    }
-}
+// Function to reset the display
+const resetPage = function () {
+  display.textContent = "";
+  console.clear();
+};
